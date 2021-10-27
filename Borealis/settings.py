5ec1525f8fd04e007808d0e5af1ef8e6b6787aa2
Anderson Ribeiro Lopes <zeander21@outlook.com.br>
2021-10-27 13:28:55 -0300

    Extra api configuration to match frontend needs

--- Borealis/settings.py
+++ Borealis/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tb50^cwby2$wfgaukj4w0z&&qtt##b(&ph^+jvv+(f)nm(^r!o'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
